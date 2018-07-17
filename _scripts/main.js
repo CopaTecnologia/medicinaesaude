316adc628ffe0218bbb38b69b81fcf64873e118c
Johnylab <joaobatistacomunic@hotmail.com>
2018-07-17 02:06:18 -0300

    Parallax em um módulo separado

--- _scripts/main.js
+++ _scripts/main.js
@@ -2,43 +2,28 @@ import SlideShow from './libs/slideshow';
 import slidingBoxes from './libs/slidingBoxes';
 import loadGoogleMaps from './libs/loadGoogleMaps';
 import bindGoogleAnalyticsEvents from './libs/bindGoogleAnalyticsEvents';
-import {
-    requestAnimFrame
-} from './libs/util';
+import parallax from './libs/parallax';
 
 document.body.addEventListener('touchstart', () => {});
 
-const parallaxContainer = document.querySelector('.parallax-container');
-if (parallaxContainer) {
-    const siteHeader = document.querySelector('.site-header');
-    const parallaxContent = parallaxContainer.querySelector('.parallax-content');
-    const maxScrollOffset = parallaxContainer.offsetTop + parallaxContainer.offsetHeight;
-
-    if (parallaxContent) {
-        window.addEventListener('gesturechange', handleScroll, {
-            passive: true
-        });
-        window.addEventListener('touchmove', handleScroll, {
-            passive: true
-        });
-        window.addEventListener('scroll', handleScroll);
-    }
-
-    function handleScroll() {
-        if (scrollY > maxScrollOffset) return false;
-        requestAnimFrame(() => {
-            parallaxContent.style.webkitTransform = `translateY(${scrollY * .5}px)`;
-            parallaxContent.style.transform = `translateY(${scrollY * .5}px)`;
-            siteHeader.style.webkitTransform = `translateY(${scrollY * .75}px)`;
-            siteHeader.style.transform = `translateY(${scrollY * .75}px)`;
-        });
-    };
-};
+SlideShow.initAll('.slideshow');
 
 slidingBoxes('.sliding-box');
 
-SlideShow.initAll('.slideshow');
-
 loadGoogleMaps();
+
+parallax(
+    '.parallax-container',
+    [
+        {
+            element: '.parallax-content',
+            amount: .5
+        },
+        {
+            element: document.querySelector('.site-header'),
+            amount: .75
+        }
+    ]
+);
 
 bindGoogleAnalyticsEvents();
